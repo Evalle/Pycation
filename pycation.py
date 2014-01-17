885b3c7bf56959e70bb57e1b0d2c2fdd08d55604
Evalle <shmarnev@gmail.com>
2014-01-17 16:14:40 +0600

    Update pycation.py

--- pycation.py
+++ pycation.py
@@ -2,8 +2,10 @@
 
 # ok, let's mortal koding begin! 
 def hotel_cost(nights):
-    return nights * 40
-
+    cost = 140 * nights
+    return cost
+    
+# check: print hotel_cost()
 def plane_ride_cost(city):
     if city == "Charlotte":
         return 183
@@ -14,16 +16,4 @@ def plane_ride_cost(city):
     elif city == "Los Angeles":
         return 475
     else:
-        return "I don't understand you, please define your city from the list below: Charlotte, Pittsburgh, Los Angeles"
-
-def rental_car_cost(days):
-    return days * 40
-    if days >= 3: 
-        return costacar - 20
-    elif days >= 7:
-        return costcar - 50
-    return costcar
-
-def trip_cost(city,days):
-    summ = hotel_cost(nights) + plane_ride_cost(city) + rental_car_cost(days)
-    return summ
+        return "I' totally don't understand you"
