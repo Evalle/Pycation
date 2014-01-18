618b032f40f82d246275bc4a40425519eb608cc7
Evalle <shmarnev@gmail.com>
2014-01-18 18:37:21 +0500

    Update pycation.py

--- pycation.py
+++ pycation.py
@@ -1,11 +1,11 @@
-# Let's compute hotel_cost 
+# Let's compute hotel_cost
 def hotel_cost(nights):
     cost = 140 * nights
-    return cost    
+    return cost
 # check block
 # print hotel_cost()
 
-# Let's compute plane ride cost 
+# Let's compute plane ride cost
 def plane_ride_cost(city):
     if city == "Charlotte":
         return 183
@@ -21,17 +21,20 @@ def plane_ride_cost(city):
 # Let's compute rental_car_cost
 def rental_car_cost(days):
     rrc = days * 40
-    if days >= 7:
+    if days >= 3 and days < 7:
         return rrc - 20
-    elif days >= 3:
+    elif days >=7:
         return rrc - 50
     else:
         return rrc
+#Check block                
+#print rental_car_cost(7)
+
 
 
 # check block
 #def new_function(nights,city):
-#    summ = hotel_cost(nights) + plane_ride_cost(city)
-#    return summ
+# summ = hotel_cost(nights) + plane_ride_cost(city)
+# return summ
 #
 # print new_function(2,"Tampa")
